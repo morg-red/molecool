b5acbecbf18f1b105a9513bf6dc05d3b63201592
Morgan Redington <morganey@buffalo.edu>
2020-01-25 14:21:45 -0500

    structure code into modules and subpackages

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,10 +1,16 @@
 """
 molecool
-A python package for viewing and analyzing xyz files
+A python package for viewing and analyzing certain files
 """
 
 # Add imports here
 from .functions import *
+from .measure import calculate_angle, calculate_distance
+from .visualize import draw_molecule, draw_bond_histogram
+from .molecule import build_bond_list
+from .atom_data import atomic_weights, atom_colors
+
+import molecool.IO
 
 # Handle versioneer
 from ._version import get_versions
